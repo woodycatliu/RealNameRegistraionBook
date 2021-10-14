b51bff0fc0fd14b969e8aab04f67df00c081265f
Woody Liu <bestlifeforzhi@gmail.com>
2021-10-15 02:51:08 +0800

    docs: remove git pod
    Former-commit-id: d796d6c4eedd56758892dd62fc8cf6cc3f5b5e2e

--- Pods/Target Support Files/Pods-RealNameRegistraionBook-RealNameRegistraionBookUITests/Pods-RealNameRegistraionBook-RealNameRegistraionBookUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RealNameRegistraionBook_RealNameRegistraionBookUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RealNameRegistraionBook_RealNameRegistraionBookUITests
-@end
