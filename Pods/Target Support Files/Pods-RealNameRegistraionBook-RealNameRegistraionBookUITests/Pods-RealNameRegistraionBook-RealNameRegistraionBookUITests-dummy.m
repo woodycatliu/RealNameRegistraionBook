668c0ae97c49ca58aaf17a68fb8679c1db47c1db
Woody Liu <bestlifeforzhi@gmail.com>
2021-10-15 03:05:49 +0800

    pod: install
    Former-commit-id: a085b6722281aad7558e7edcff0e32d1fde4496c

---
+++ Pods/Target Support Files/Pods-RealNameRegistraionBook-RealNameRegistraionBookUITests/Pods-RealNameRegistraionBook-RealNameRegistraionBookUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealNameRegistraionBook_RealNameRegistraionBookUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RealNameRegistraionBook_RealNameRegistraionBookUITests
+@end
